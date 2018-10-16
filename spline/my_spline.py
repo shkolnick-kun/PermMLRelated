a62981bdcbc0df5a94be67ed51977cd683abf15b
shkolnick-kun <shkolnick.kun@gmail.com>
2018-10-16 23:31:37 +0500

    Update my_spline.py

--- spline/my_spline.py
+++ spline/my_spline.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sat Oct 13 18:52:59 2018
-
-@author: anon
+Spline interpolation and least squares prototypes.
+Copyright (c) 2018 Paul Beltyukov (beltyukov.p.a@gmail.com)
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 """
 """
 http://www.machinelearning.ru/wiki/index.php?title=%D0%98%D0%BD%D1%82%D0%B5%D1%80%D0%BF%D0%BE%D0%BB%D1%8F%D1%86%D0%B8%D1%8F_%D0%BA%D1%83%D0%B1%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B8%D0%BC%D0%B8_%D1%81%D0%BF%D0%BB%D0%B0%D0%B9%D0%BD%D0%B0%D0%BC%D0%B8
@@ -11,47 +25,6 @@ https://ru.wikipedia.org/wiki/%D0%9C%D0%B5%D1%82%D0%BE%D0%B4_%D0%BF%D1%80%D0%BE%
 """
 import numpy as np
 from bisect import bisect_right
-
-'''
-def tridiag_solve_out(A, B, C, F):
-    n = len(F)
-    
-    if n < 2:
-        return None
-    
-    if len(C) != n:
-        return None
-    
-    if len(A) != n - 1:
-        return None
-    
-    if len(B) != n - 1:
-        return None
-    
-    alpha = np.zeros(A.shape)
-    beta  = np.zeros(A.shape)
-    
-    alpha[0] = -B[0] / C[0]
-    beta[0]  =  F[0] / C[0]
-    
-    for i in range(1, n - 1):
-        d = (A[i - 1] * alpha[i - 1] + C[i])
-        alpha[i] = -B[i] / d
-        beta[i]  = (F[i] - A[i - 1] * beta[i - 1]) / d
-        
-    x = np.zeros(F.shape)
-    
-    x[n - 1] = (F[n - 1] - A[n - 2] * beta[n - 2])
-    x[n - 1] = x[n - 1] / (A[n - 2] * alpha[n - 2] + C[n - 1]) 
-    
-    for i in range(n - 2, -1, -1):
-        x[i] = x[i + 1] * alpha[i] + beta[i]
-        
-    print(alpha)
-    print(beta)
-    
-    return x
-'''
 """
 Solve Mx = F where M is tridiag(A,C,B) inplace
 
@@ -156,6 +129,8 @@ class my_spline(object):
         y = np.zeros(X.shape, X.dtype)
         for i in range(0, len(X)):
             j = bisect_right(self.x, X[i]) - 1
+            if j < 0:
+                j = 0
             d = X[i] - self.x[j]
             y[i] = self.a[j] + d * (self.b[j] + d * (self.c[j] + d * self.d[j]))
             
@@ -185,73 +160,6 @@ z = si.predict(X)
 import matplotlib.pyplot as plt
 
 plt.plot(X,y,X,z)
-'''
-'''
-Solve Mx = F inplace 
-where M is tridiag(A,C,B), x and F ar rect matrices 
-
-first:
-Alpha -> C
-Beta  -> F
-
-then:
-x     -> F
-
-returns x
-'''
-'''
-def tridiag_solve_mtx_in(A, B, C, F):
-    n = len(C)
-    
-    if n < 2:
-        raise ValueError
-    
-    if F.shape[0] != n:
-        raise ValueError
-    
-    if len(A) != n - 1:
-        raise ValueError
-    
-    if len(B) != n - 1:
-        raise ValueError
-    
-    d    = C[0]
-    C[0] = -B[0] / d     #Alpha[0]
-    F[0,:] =  F[0,:] / d #Beta[0,:]
-    
-    for i in range(1, n - 1):
-        d    = (A[i - 1] * C[i - 1] + C[i])
-        C[i] = -B[i] / d                              #Alpha[i]
-        F[i,:] = (F[i,:] - A[i - 1] * F[i - 1,:]) / d #Beta[i,:]
-        
-    F[n - 1,:] = (F[n - 1,:] - A[n - 2] * F[n - 2,:]) / (A[n - 2] * C[n - 2] + C[n - 1])
-    
-    for i in range(n - 2, -1, -1):
-        F[i,:] = F[i + 1,:] * C[i] + F[i,:]
-    
-    return F
-'''
-'''
-A = np.array([1,2,7], dtype='float')
-B = np.array([3,1,1], dtype='float')
-C = np.array([2,2,2,2], dtype='float')
-F = np.array([[1,3],[1,2],[1,1],[1,0]], dtype='float')
-
-c0 = C.copy()
-f0 = F[:,0].copy()
-c1 = C.copy()
-f1 = F[:,1].copy()
-
-x = tridiag_solve_in(A,B,c0,f0)
-x = tridiag_solve_in(A,B,c1,f1)
-print(f0)
-print(f1)
-
-x = tridiag_solve_mtx_in(A,B,C,F)
-print(A)
-print(B)
-print(C)
-print(F)
 '''
 class my_spline_lsq(object):
     
@@ -324,6 +232,8 @@ class my_spline_lsq(object):
         y = np.zeros(X.shape, X.dtype)
         for i in range(0, len(X)):
             j = bisect_right(self.x, X[i]) - 1
+            if j < 0:
+                j = 0
             d = X[i] - self.x[j]
             y[i] = self.a[j] + d * (self.b[j] + d * (self.c[j] + d * self.d[j]))
             
@@ -334,13 +244,17 @@ import matplotlib.pyplot as plt
 
 X = np.arange(0.0, 10.0, 0.05)
 
-bs = my_spline_lsq(X[::30])
+lss = my_spline_lsq(X[::30])
+s = my_spline()
 
-X = np.arange(0.0, 10.0, 0.05)
 y = np.sin(X)
 
-bs.fit(X,y)
+s.fit(X[::30], y[::30])
+lss.fit(X,y)
 
-plt.plot(X,y, X,bs.predict(X))
+sp = s.predict(X)
+lssp = lss.predict(X)
+
+plt.plot(X,y, X,sp, X,lssp)
+plt.plot(X,y-sp, X,y-lssp)
 '''
-
